095f40573cbe4966287d408b73ac786fd92ef372
Joey Davidson <j.davidson18@gmail.com>
2021-11-18 17:51:10 -0700

    home component created, background image set from module.css

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { Counter } from "./features/counter/Counter";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Header2 from "./components/Header2/Header2";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div className="App">
       {/* <Header /> */}
       <Header2 />
+      <Home />
     </div>
   );
 }
---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Home.module.css";
+import Image from "../../images/frames1.jpeg";
+
+function Home() {
+  return (
+    <div>
+      <div className={styles["mainContain"]}></div>
+    </div>
+  );
+}
+
+export default Home;
