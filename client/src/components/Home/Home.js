bb13ef99de393972f28a5877d80e2e25c6af5ab8
Joey Davidson <j.davidson18@gmail.com>
2021-11-19 03:38:25 -0700

    grid established for body, buttons created

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -6,41 +6,30 @@ import styled from "styled-components";
 function Home() {
   //   let tempBack = "frame1";
 
-  const [tempBack, setTempBack] = useState("frame1");
+  const [tempBack, setTempBack] = useState("frames.jpeg");
 
-  useEffect(() => {
-    timerBounce();
-  }, []);
-
-  const timerBounce = (backgroundImage) => {
-    const timer = setTimeout(() => {
-      setTempBack("frame1");
-      timerBounce2();
-    }, 10000);
-  };
-  const timerBounce2 = (backgroundImage) => {
-    const timer2 = setTimeout(() => {
-      setTempBack("frame2");
-      timerBounce3();
-    }, 10000);
-  };
-  const timerBounce3 = (backgroundImage) => {
-    const timer2 = setTimeout(() => {
-      setTempBack("frame3");
-      timerBounce4();
-    }, 10000);
-  };
-  const timerBounce4 = (backgroundImage) => {
-    const timer2 = setTimeout(() => {
-      setTempBack("frame4");
-      timerBounce();
-    }, 10000);
-  };
+  useEffect(() => {}, []);
 
   return (
     <div>
-      <div className={styles[`${tempBack}`]}></div>
-      {/* <Container></Container> */}
+      {/* <div className={styles[`${tempBack}`]}></div> */}
+      <Container bgImage={tempBack}>
+        <div className={styles["myWrap"]}>
+          <p>Frame it UP</p>
+        </div>
+        <div className={styles["myWrap"]}>
+          <p>How it Works</p>
+        </div>
+        <div className={styles["myWrap"]}>
+          <p>Upload Image</p>
+        </div>
+        <div className={styles["myWrap"]}>
+          <p>Custom Frames</p>
+        </div>
+        <div className={styles["myWrap"]}>
+          <p>Gift Ideas</p>
+        </div>
+      </Container>
     </div>
   );
 }
@@ -49,8 +38,14 @@ export default Home;
 
 const Container = styled.div`
   min-height: calc(100vh - 120px);
-  background-image: url("/images/frames1.jpeg");
+  background-image: ${(props) => `url("/images/${props.bgImage}")`};
   background-position: top;
   background-repeat: no-repeat;
   background-size: cover;
+  display: grid;
+  grid-template: repeat(5, minmax(0, 1fr));
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  grid-gap: 25px;
 `;
